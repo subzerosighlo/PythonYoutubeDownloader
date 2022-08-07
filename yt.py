71eb254cc7ca2808e5c56fdb365d9011656255ce
SubzeroSighlo <garynagar@icloud.com>
2022-08-07 11:41:25 -0400

    Added progressbar and more logic for user input

--- yt.py
+++ yt.py
@@ -1,17 +1,28 @@
+import progressbar as progress
 from pytube import YouTube
 from sys import argv
+from pytube.cli import on_progress
 
 
 welcomeMessage = 'Sighlos Youtube Video Downloader'
 new_str = welcomeMessage.center(20)
 print(new_str)
+
+
 link = input("Enter Youtube link: ")
 
-yt = YouTube(link)
+yt = YouTube(link, on_progress_callback=on_progress)
 
-print("Title: ", yt.title)
+print("Is this the video you are trying to download? ", yt.title)
+userinput = input("Enter yes or no: ")
+if userinput == "yes":
 
-yd = yt.streams.get_highest_resolution()
-filelocation = input("Enter your file location: ")
+    yd = yt.streams.get_highest_resolution()
+    filelocation = input("Enter your file location: ")
 
-yd.download(filelocation)
+    yd.download(filelocation)
+    print("Finished Downloading ", yt.title)
+else:
+    print("We are having issues")
+    print("Exiting now")
+    exit
